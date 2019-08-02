623fd02dcfc43a86032d25b9f662cf77215f634a
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-02 20:40:44 +0300

    Oldest Family Member

---
+++ Defining Classes/Oldest Family Member/Family.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> members;
+
+        public Family()
+        {
+            this.members = new List<Person>();
+        }
+        public void AddMember(Person member)
+        {
+            members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestPerson = members.OrderByDescending(x => x.Age).FirstOrDefault();
+            return oldestPerson;
+        }
+    }
+}
---
+++ Defining Classes/Oldest Family Member/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i< n; i++)
+            {
+                var input = Console.ReadLine().Split().ToArray();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                var membo = new Person(input[0], age);
+                family.AddMember(membo);
+            }
+            Console.WriteLine($"{family.GetOldestMember().Name} {family.GetOldestMember().Age}");
+            //foreach (var item in family.Members)
+            //{ 
+            //    Console.WriteLine(item.Name);
+            //    Console.WriteLine(item.Age);
+            //}
+
+        }
+    }
+}
