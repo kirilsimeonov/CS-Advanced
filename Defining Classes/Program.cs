919217e37461f2e4fcbc12ff57aca633d6ed7f7a
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-02 20:38:08 +0300

    Defining Classes 1

---
+++ Defining Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace DefiningClasses
+{
+    public class StartUp
+
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Kiril";
+            person.Age = 34;
+
+            Person person2 = new Person("Misho", 35);
+
+            Console.WriteLine($"Name:{person.Name} Age:{person.Age} ");
+            Console.WriteLine($"Name:{person2.Name} Age:{person2.Age} ");
+
+        }
+    }
+}
