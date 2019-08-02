b7e1a89b1d0a98a4123168d250a8b1f59cee0259
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-02 20:39:31 +0300

    Defining Classes 2

---
+++ Defining Classes/Creating Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace DefiningClasses
+{
+    public class StartUp
+
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Kiril";
+            person.Age = 34;
+
+            Person person2 = new Person("Misho", 35);
+            Person person3 = new Person(33);
+
+            Console.WriteLine($"Name:{person.Name} Age:{person.Age} ");
+            Console.WriteLine($"Name:{person2.Name} Age:{person2.Age} ");
+            Console.WriteLine($"Name:{person3.Name} Age:{person3.Age} ");
+
+        }
+    }
+}
