5dc81a2f0ba712a74ec3e6220d7b32c7819667c9
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-02 20:41:44 +0300

    Opinion Poll

---
+++ Defining Classes/Opinion Poll/Family.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> members;
+
+        public Family()
+        {
+            this.members = new List<Person>();
+        }
+        public void AddMember(Person member)
+        {
+            members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestPerson = members.OrderByDescending(x => x.Age).FirstOrDefault();
+            return oldestPerson;
+        }
+        public List<Person> OlderThan30()
+        {
+             
+            List<Person> older = members.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+            return older;
+
+        }
+    }
+}
---
+++ Defining Classes/Opinion Poll/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i< n; i++)
+            {
+                var input = Console.ReadLine().Split().ToArray();
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                var membo = new Person(input[0], age);
+                family.AddMember(membo);
+            }
+
+            List<Person> older = new List<Person>();
+            older = family.OlderThan30();
+            foreach (var item in older)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+
+            //Console.WriteLine($"{family.GetOldestMember().Name} {family.GetOldestMember().Age}");
+            //foreach (var item in family.Members)
+            //{ 
+            //    Console.WriteLine(item.Name);
+            //    Console.WriteLine(item.Age);
+            //}
+
+        }
+    }
+}
