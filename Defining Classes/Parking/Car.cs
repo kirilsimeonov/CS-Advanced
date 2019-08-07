46454de6ed2c477003eb1b3453b48a927facf55e
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-07 22:33:28 +0300

    Parking

---
+++ Defining Classes/Parking/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+namespace SoftUniParking
+{
+    public class Car
+    {
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public string RegistrationNumber { get; set; }
+
+
+
+       
+        public Car(string make, string model, int hp,string registration)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = hp;
+            this.RegistrationNumber = registration;
+        }
+
+        //public override string ToString()
+        //{
+        //    StringBuilder sb = new StringBuilder();
+
+        //    sb.AppendLine($"Make: {this.Make}");
+        //    sb.AppendLine($"Model: {this.Model}");
+        //    sb.AppendLine($"HorsePower: {this.HorsePower}");
+        //    sb.AppendLine($"RegistrationNumber: {this.RegistrationNumber}");
+
+        //    return sb.ToString().Trim();
+        //}
+        public override string ToString()
+        {
+            //string result = $"Make: {Make}" +
+            //    Environment.NewLine +
+            //    $"Model: {Model}" +
+            //    Environment.NewLine +
+            //    $"HorsePower: {HorsePower}" +
+            //    Environment.NewLine	+
+            //    $"RegistrationNumber: {RegistrationNumber}";
+
+            //return result;
+
+
+            return $"Make: {this.Make}" + Environment.NewLine + $"Model: {this.Model}" +
+                Environment.NewLine+ $"HorsePower: {this.HorsePower}" +
+                    Environment.NewLine + $"RegistrationNumber: {this.RegistrationNumber}";
+        }
+    }
+}
---
+++ Defining Classes/Parking/Parking.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+
+
+        private int capacity;
+
+        public List<Car> Cars { get; set; }
+
+        public int Count { get {
+                return Cars.Count();
+            }
+         }
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+            Cars = new List<Car>();
+        }
+
+        public string AddCar (Car car)
+        {
+
+
+
+
+                if (Cars.Any(x => x.RegistrationNumber == car.RegistrationNumber))
+                {
+                    return "Car with that registration number, already exists!";
+                }
+                else if (Cars.Count == capacity)
+                {
+                    return "Parking is full!";
+                }
+                else
+                {
+                    Cars.Add(car);
+                    return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+                }
+            
+        }
+        public string RemoveCar (string registrationNumber)
+        {
+            if (Cars.Any(x=>x.RegistrationNumber==registrationNumber))
+            {
+                Cars.RemoveAll(x => x.RegistrationNumber == registrationNumber);
+                return $"Successfully removed {registrationNumber}";
+            }
+            else
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+        }
+        public Car GetCar(string registrationNumber)
+        {
+            Car car1 = Cars.Where(x => x.RegistrationNumber == registrationNumber).FirstOrDefault();
+            return car1;
+        }
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var item in registrationNumbers)
+            {
+                if (Cars.Any(x=>x.RegistrationNumber==item))
+                {
+                    Cars.RemoveAll(x => x.RegistrationNumber == item);
+                }
+            }
+        }
+    }
+}
