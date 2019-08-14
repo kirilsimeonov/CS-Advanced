9df7207bb14ef9f206f8ec469544d47df7f16112
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-14 12:57:24 +0300

    Generic Swap Box String

---
+++ Generics/Generic Swap Method String/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+
+   
+
+    public class Box<T>
+    {
+        public List<T> Data { get; set; } = new List<T>();
+
+
+
+
+
+        public void Add(T value)
+        {
+            this.Data.Add(value);
+        }
+    }
+}
---
+++ Generics/Generic Swap Method String/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericSwapMethodStrings
+{
+
+    public class StartUp
+    {
+        private static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Box<string> list = new Box<string>();
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+
+            int[] command = Console.ReadLine().Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            
+            int firstIndex = command[0];
+            int secondIndex = command[1];
+
+
+
+            Switch(list.Data, firstIndex, secondIndex);
+
+            list.Data.ForEach(x => Console.WriteLine($"{x.GetType()}: {x}"));
+        }
+
+
+
+
+
+        private static void Switch<T>(List<T> list, int firstIndex, int secondIndex)
+        {
+            T temp = list[firstIndex];
+
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+        }
+    }
+}
