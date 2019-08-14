04fb39e0ec976e75c2a03829020189a08fbd18a9
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-14 12:55:38 +0300

    Generic Box of Integer

---
+++ Generics/Generic Box of Integer/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        public T Value { get; set; }
+
+
+
+
+
+
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Value}";
+        }
+    }
+}
---
+++ Generics/Generic Box of Integer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        private static void Main()
+        {
+
+
+
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<int>> list = new List<Box<int>>();
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+
+                Box<int> input = new Box<int>()
+
+                { Value = int.Parse(Console.ReadLine()) };
+
+                list.Add(input);
+            }
+
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
