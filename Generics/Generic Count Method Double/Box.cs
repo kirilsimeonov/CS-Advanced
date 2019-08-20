1e174cebc7ba68a20279e43adaa671ab8d26045f
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-20 22:42:09 +0300

    Generic Count Method Double

---
+++ Generics/Generic Count Method Double/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+
+namespace GenericCountMethodDoubles
+{
+  
+
+    public class Box<T>
+    {
+        public List<T> Data { get; set; } = new List<T>();
+
+        public void Add(T value)
+        {
+            this.Data.Add(value);
+        }
+    }
+}
---
+++ Generics/Generic Count Method Double/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+namespace GenericCountMethodDoubles
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Box<double> box = new Box<double>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                box.Add(double.Parse(Console.ReadLine()));
+            }
+
+            double num = double.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine(Compare(box.Data, num));
+        }
+
+        private static object Compare<T>(List<T> boxData, double number)
+        {
+            int count = 0;
+
+            foreach (var item in boxData)
+            {
+
+                if (number.CompareTo(item) < 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
