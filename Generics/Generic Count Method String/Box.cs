81faa33fad26eba4480efca64900dc36c27cca5f
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-20 22:40:46 +0300

    Generic Count Method String

---
+++ Generics/Generic Count Method String/Box.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCountMethodString
+{
+    public class Box<T> where T : IComparable
+    {
+        private List<T> internalList;
+
+        public Box()
+        {
+            internalList = new List<T>();
+        }
+        public void Add(T element)
+        {
+            internalList.Add(element);
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            T temp = internalList[firstIndex];
+            internalList[firstIndex] = internalList[secondIndex];
+            internalList[secondIndex] = temp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in internalList)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return sb.ToString();
+        }
+
+        public int GetCountOfGreaterElements(T element)
+        {
+            int greaterElements = 0;
+            foreach (var item in internalList)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    greaterElements++;
+                }
+            }
+
+            return greaterElements;
+        }
+    }
+}
---
+++ Generics/Generic Count Method String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GenericCountMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+
+            var list = new Box<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+
+            string item = Console.ReadLine();
+
+
+
+
+            Console.WriteLine(list.GetCountOfGreaterElements(item));
+        }
+    }
+}
