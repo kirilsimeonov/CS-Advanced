36c7867bd6b3edbd1464f32304e459a8a3f43684
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-20 22:39:33 +0300

    Generic Swap Method Integer

---
+++ Generics/Generic Swap Method Integer/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodIntegers
+{
+    public class Box<T>
+    {
+        private List<T> internalList;
+
+        public Box()
+        {
+            internalList = new List<T>();
+        }
+        public void Add(T element)
+        {
+            internalList.Add(element);
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            T temp = internalList[firstIndex];
+            internalList[firstIndex] = internalList[secondIndex];
+            internalList[secondIndex] = temp;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in internalList)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Generics/Generic Swap Method Integer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodIntegers
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var list = new Box<int>();
+
+            int entries = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < entries; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            list.Swap(indexes[0], indexes[1]);
+
+            Console.WriteLine(list.ToString());
+        }
+    }
+}
