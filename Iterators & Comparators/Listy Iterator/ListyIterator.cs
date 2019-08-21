091e5e4d63af5ba75cb14dc486170bcc31f25b38
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-21 14:49:07 +0300

    Listy Iterator

---
+++ Iterators & Comparators/Listy Iterator/ListyIterator.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+
+
+
+
+
+    public class ListyIterator<T>
+    {
+
+
+
+        public ListyIterator(params T[] data)
+        {
+
+
+            Data = data.ToList();
+        }
+
+
+
+
+
+        public int Index { get; set; } = 0;
+
+        private List<T> Data { get; }
+
+
+
+
+        public void Create(params T[] elements)
+        {
+            for (int i = 1; i < elements.Length; i++)
+            {
+                this.Data.Add(elements[i]);
+            }
+        }
+
+
+        public void Print()
+        {
+            if (Data.Count != 0)
+            {
+               
+
+                  Console.WriteLine(Data[Index]);
+            }
+            else
+            {
+                Console.WriteLine($"Invalid Operation!");
+            }
+        }
+        public bool Move()
+        {
+            if (Next())
+            {
+
+
+                Index++;
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+
+        public bool Next()
+        {
+            return Index + 1 < this.Data.Count;
+        }
+
+       
+    }
+}
---
+++ Iterators & Comparators/Listy Iterator/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ListyIterator
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var data = new ListyIterator<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+
+
+                if (input.Contains("Create"))
+                {
+                    data.Create(input.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries));
+                }
+               
+
+                 
+                   else switch (input)
+                    {
+                        case "Move":
+                            Console.WriteLine(data.Move());
+                            break;
+
+                       
+
+                        case "Print":
+                            data.Print();
+                            break;
+
+                        case "HasNext":
+                            Console.WriteLine(data.Next());
+                            break;
+                    }
+            }
+        }
+    }
+}
