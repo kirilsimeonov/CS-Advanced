f6cf884dcdf3b9d1fdc70a22d65a0223e0220a5f
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-22 14:23:55 +0300

    Stack

---
+++ Iterators & Comparators/Stack/MyStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+
+        public void Push(T element)
+        {
+            collection.Add(element);
+        }
+
+        public MyStack()
+        {
+            collection = new List<T>();
+        }
+
+
+
+       
+        public void Pop()
+        {
+
+
+
+            if (collection.Count > 0)
+            {
+                collection.RemoveAt(collection.Count - 1);
+            }
+            else
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = collection.Count - 1; i >= 0; i--)
+            {
+                yield return collection[i];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = collection.Count - 1; i >= 0; i--)
+            {
+                yield return collection[i];
+            }
+        }
+
+       
+    }
+}
---
+++ Iterators & Comparators/Stack/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+
+            MyStack<string> collection = new MyStack<string>();
+
+
+
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+
+                string[] line = input.Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries);
+                string command = line[0];
+
+                try
+                {
+                    if (command == "Push")
+                    {
+                        for (int i = 1; i < line.Length; i++)
+                        {
+                            collection.Push(line[i]);
+                        }
+                    }
+
+
+                    else if (command == "Pop")
+                    {
+                        collection.Pop();
+                    }
+                }
+
+
+
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
