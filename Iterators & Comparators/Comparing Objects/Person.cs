bd0f8693ea540422fe823393ad95ff034fd4df4c
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-22 14:25:21 +0300

    Comparing Objects

---
+++ Iterators & Comparators/Comparing Objects/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ComparingObjects
+{
+    class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public Person(string name, int age, string town)
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+
+        public int CompareTo(Person other)
+        {
+
+
+            int result = this.name.CompareTo(other.name);
+
+
+            if (result == 0)
+            {
+                result = this.age.CompareTo(other.age);
+            }
+
+
+
+
+            if (result == 0)
+            {
+                result = this.town.CompareTo(other.town);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Iterators & Comparators/Comparing Objects/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>();
+
+
+
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split();
+                if (command[0] == "END")
+                {
+                    break;
+                }
+
+
+
+                people.Add(new Person(command[0], int.Parse(command[1]), command[2]));
+            }
+
+
+
+
+            int index = int.Parse(Console.ReadLine());
+            if (index > people.Count - 1)
+            {
+                Console.WriteLine("No matches");
+                return;
+            }
+            var personToCompare = people[index];
+
+            int diffPerson = 0;
+            int samePerson = 0;
+
+
+
+
+            for (int i = 0; i < people.Count; i++)
+            {
+
+                if (personToCompare.CompareTo(people[i]) == 0)
+                {
+
+                    samePerson++;
+                         }
+                        else
+                      {
+                    diffPerson++;
+                     }
+            }
+
+
+
+
+
+            if (samePerson > 0)
+            {
+                Console.WriteLine($"{samePerson} {diffPerson} {people.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
