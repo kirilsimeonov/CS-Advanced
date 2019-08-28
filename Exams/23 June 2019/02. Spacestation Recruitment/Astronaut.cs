318c0345b7bd5123a8d245ebc4915ea4b8611047
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-08-28 17:40:25 +0300

    SpacestationRecruitment

---
+++ Exams/23 June 2019/02. Spacestation Recruitment/Astronaut.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SpaceStationRecruitment
+{
+    public class Astronaut
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+
+        public Astronaut(string name, int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public override string ToString()
+        {
+
+            string result = $"Astronaut: {Name}, {Age} ({Country})";
+
+            return result;
+        }
+    }
+}
---
+++ Exams/23 June 2019/02. Spacestation Recruitment/SpaceStation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace SpaceStationRecruitment
+{
+    public class SpaceStation
+    {
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        private List<Astronaut> data;
+
+        public int Count { get {
+                return data.Count();
+        } }
+
+        public SpaceStation(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Astronaut>();
+        }
+
+        public void Add(Astronaut astrounaut)
+        {
+            if (data.Count<Capacity)
+            {
+                data.Add(astrounaut);
+            }
+        }
+        public bool Remove(string name)
+        {
+            if (data.Any(x=>x.Name==name))
+            {
+                var current = data.Where(x => x.Name == name).FirstOrDefault();
+                data.Remove(current);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Astronaut GetOldestAstronaut()
+        {
+            var oldest = data.OrderByDescending(x => x.Age).FirstOrDefault();
+                return oldest;
+        }
+        public Astronaut GetAstronaut (string name)
+        {
+            var current = data.Where(x => x.Name == name).FirstOrDefault();
+            return current;
+        }
+        public string Report ()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Astronauts working at Space Station {Name}:");
+            foreach (var item in data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString().Trim();
+
+        }
+
+    }
+}
