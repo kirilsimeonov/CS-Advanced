abfb98a2fafcb9786f1c3053a2f818e5ae2a0362
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-09-03 14:13:57 +0300

    Heroes

---
+++ Exams/24 Feb/03 Heroes/Hero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Heroes
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public Item Item { get; set; }
+       
+         public Hero(string name, int level, Item item)
+        {
+            Name = name;
+            Level = level;
+            Item = item;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder heroResult = new StringBuilder();
+            heroResult.AppendLine($"Hero: {Name} - {Level}lvl");
+
+            heroResult.AppendLine(Item.ToString());
+
+
+            return heroResult.ToString().Trim();
+        }
+    }
+}
---
+++ Exams/24 Feb/03 Heroes/HeroRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes
+{
+    public class HeroRepository
+    {
+        private List<Hero> repository;
+
+
+        public int Count
+        {
+            get
+            {
+                return repository.Count;
+            }
+        }
+
+        public HeroRepository()
+        {
+            repository = new List<Hero>();
+        }
+        public void Add(Hero hero)
+        {
+            repository.Add(hero);
+        }
+        public void Remove(string name)
+        {
+            if (repository.Any(x=>x.Name==name))
+            {
+                repository.RemoveAll(x => x.Name == name);
+            }
+        }
+        public Hero GetHeroWithHighestStrength()
+        {
+            var highest = repository.OrderByDescending(x => x.Item.Strength).FirstOrDefault();
+            return highest;
+
+        }
+        public Hero GetHeroWithHighestIntelligence()
+        {
+            var highest = repository.OrderByDescending(x => x.Item.Intelligence).FirstOrDefault();
+            return highest;
+
+        }
+        public Hero GetHeroWithHighestAbility()
+        {
+            var highest = repository.OrderByDescending(x => x.Item.Ability).FirstOrDefault();
+            return highest;
+
+        }
+        public override string ToString()
+        {
+            StringBuilder heroes = new StringBuilder();
+
+            foreach (var itemm in repository)
+            {
+                heroes.AppendLine(itemm.ToString().Trim());
+            }
+            return heroes.ToString().Trim();
+        }
+
+    }
+
+
+}
