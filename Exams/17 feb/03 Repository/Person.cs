b9a77910d7bb7975b118237d5e97a4ef2b6af071
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-09-04 20:33:19 +0300

    Repository

---
+++ Exams/17 feb/03 Repository/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Repository
+{
+    public class Person
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthdate { get; set; }
+
+
+
+        public Person(string name, int age, DateTime datetime)
+        {
+            Name = name;
+            Age = age;
+            Birthdate = datetime;
+        }
+    }
+}
---
+++ Exams/17 feb/03 Repository/Repository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Repository
+{
+    public class Repository
+    {
+
+        private List<Person> data;
+
+        public int Count
+        {
+            get
+            { return data.Count(); }
+        }
+
+        public Repository()
+        {
+            data = new List<Person>();
+        }
+        public void Add (Person person)
+        {
+            data.Add(person);
+         }
+        public Person Get (int id)
+        {
+            return data[id];
+        }
+        public bool Update (int id, Person newperson)
+        {
+            if (data.Count>id)
+            {
+
+                data[id] = newperson;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Delete (int id)
+        {
+            if (data.Count>id)
+            {
+                data.RemoveAt(id);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
+
